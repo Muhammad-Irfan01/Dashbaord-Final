412067b288ebb4ca4bf69d8c4a24e5a39a0e4002
Muhammad-Irfan01 <irfan.bvoir@gmail.com>
2024-11-22 15:14:38 +0500

    www

---
+++ shopify-dashboard-art-gallery/dist/src-dashboard/assets/useOutsideClick-CPcDmwY5.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-SqxPX7HF.js";const u=({ref:e,handler:t})=>{s.useEffect(()=>{const n=o=>{e.current&&!e.current.contains(o.target)&&t()};return document.addEventListener("mousedown",n),()=>{document.removeEventListener("mousedown",n)}},[e,t])};export{u};
